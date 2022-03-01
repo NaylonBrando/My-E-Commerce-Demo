af7a24c6f00127c4d3728d6e889f7088866fa685
Erhan Rohat Turuncu <rohatturuncu@gmail.com>
2022-03-02 02:27:45 +0300

    load eklendi

---
+++ load.php
@@ -0,0 +1,9 @@
+<?php
+require_once 'Connection.php';
+require_once __DIR__ . '/controller/Homepage.php';
+require_once __DIR__ . '/src/entity/User.php';
+require_once __DIR__ . '/src/repository/UserRepository.php';
+require_once __DIR__ . '/controller/AbstractController.php';
+require_once __DIR__ . '/controller/UserController.php';
+require_once __DIR__ . '/controller/Database.php';
+require_once __DIR__ . '/router.php';
