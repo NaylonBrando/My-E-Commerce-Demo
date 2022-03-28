f10d6d94af80bd2ad0c20b75912ea3ff42d742d2
Erhan Rohat Turuncu <rohatturuncu@gmail.com>
2022-03-29 01:58:33 +0300

    boostrap güncellendi

--- bootstrap.php
+++ bootstrap.php
@@ -1,9 +1,9 @@
 <?php
+use Router\Router;
+
 session_start();
 require_once __DIR__ . "/load.php";
 
-$uri = $_SERVER['REQUEST_URI'];
-
 $routerEntity = new Router();
-$routerEntity->run($uri);
-?>
+$routerEntity->run();
+
