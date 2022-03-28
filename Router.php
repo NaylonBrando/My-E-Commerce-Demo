312eb8bcff8043a5f7e6273441e916935554390c
Erhan Rohat Turuncu <rohatturuncu@gmail.com>
2022-03-29 01:57:33 +0300

    Router namespace refactor
    Routere namespace eklendi. Ek olarak controllerlere eklenecek namespaceler icin refactor yapıldı

--- Router.php
+++ Router.php
@@ -1,105 +1,109 @@
 <?php
-
-$routes = [
-    "homepage" => [
-        "url" => "/",
-        "class" => "HomepageController",
-        "function" => "index",
-        "type" => "normal",
-        "template" => "homepage.php"
-    ],
-    "login" => [
-        "url" => "/login",
-        "class" => "UserController",
-        "function" => "login",
-        "type" => "normal",
-        "template" => "login.php"
-    ],
-    "check-login"=>[
-        "url" => "/check-login",
-        "class" => "UserController",
-        "function" => "loginCheck",
-        "type" => "check"
-    ],
-    "logout"=>[
-        "url" => "/logout",
-        "class" => "UserController",
-        "function" => "logout",
-        "type" => "check"
-    ],
-    "register"=>[
-        "url" => "/register",
-        "class" => "UserController",
-        "function" => "register",
-        "type" => "normal",
-        "template" => "register.php"
-    ],
-    "check-register"=>[
-        "url" => "/check-register",
-        "class" => "UserController",
-        "function" => "registerCheck",
-        "type" => "check"
-    ],
-    "product-list"=>[
-        "url" => "/product-list",
-        "class" => "ProductController",
-        "function" => "listProducts",
-        "type" => "normal",
-        "template" =>"product.php"
-    ],
-    "category"=>[
-        "url" => "/category",
-        "class" => "CategoryController",
-        "function" => "show",
-        "type" => "normal",
-        "template" =>"category.php"
-    ],
-    "add-category"=>[
-        "url" => "/add-category",
-        "class" => "CategoryController",
-        "function" => "add",
-        "type" => "check",
-    ],
-    "delete-category"=>[
-        "url" => "/delete-category",
-        "class" => "CategoryController",
-        "function" => "delete",
-        "type" => "check",
-    ],
-    "update-category"=>[
-        "url" => "/update-category/{id}",
-        "class" => "CategoryController",
-        "function" => "updateShow",
-        "type" => "normal",
-        "template" =>"updateCategory.php"
-    ],
-    "update-category-check"=>[
-        "url" => "/update-category-check",
-        "class" => "CategoryController",
-        "function" => "updateCheck",
-        "type" => "check",
-    ],
+namespace Router;
 
 
-];
-
-
-//admin router
+//son kullanici router
 class Router
 {
 
-
     public function run()
     {
-        global $routes;
+        $routes = [
+            "homepage" => [
+                "url" => "/",
+                "class" => "HomepageController",
+                "function" => "index",
+                "type" => "normal",
+                "template" => "homepage.php"
+            ],
+            "login" => [
+                "url" => "/login",
+                "class" => "UserController",
+                "function" => "login",
+                "type" => "normal",
+                "template" => "login.php"
+            ],
+            "check-login" => [
+                "url" => "/check-login",
+                "class" => "UserController",
+                "function" => "loginCheck",
+                "type" => "check"
+            ],
+            "logout" => [
+                "url" => "/logout",
+                "class" => "UserController",
+                "function" => "logout",
+                "type" => "check"
+            ],
+            "register" => [
+                "url" => "/register",
+                "class" => "UserController",
+                "function" => "register",
+                "type" => "normal",
+                "template" => "register.php"
+            ],
+            "check-register" => [
+                "url" => "/check-register",
+                "class" => "UserController",
+                "function" => "registerCheck",
+                "type" => "check"
+            ],
+            "profile" => [
+                "url" => "/profile",
+                "class" => "UserController",
+                "function" => "profile",
+                "type" => "normal",
+                "template" => "profile.php"
+            ],
+            "check-update-profile" => [
+                "url" => "/check-update-profile",
+                "class" => "UserController",
+                "function" => "update",
+                "type" => "check"
+            ],
+            "check-update-password" => [
+                "url" => "/check-update-password",
+                "class" => "UserController",
+                "function" => "updatePassword",
+                "type" => "check"
+            ],
+            "shoppingCart" => [
+                "url" => "/cart",
+                "class" => "CartController",
+                "function" => "show",
+                "type" => "normal",
+                "template" => "cart.php",
+            ],
+            "check-add-product-to-cart" => [
+                "url" => "/check-add-product-to-cart",
+                "class" => "CartController",
+                "function" => "add",
+                "type" => "check",
+            ],
+            "check-delete-product-from-cart" => [
+                "url" => "/check-delete-product-from-cart/{id}",
+                "class" => "CartController",
+                "function" => "delete",
+                "type" => "check",
+            ],
+            "product-slug" => [
+                "url" => "/{productSlug}",
+                "class" => "ProductController",
+                "function" => "show",
+                "type" => "normal",
+                "template" => "product.php",
+            ],
+        ];
 
+        $match = false;
         $request_uri = self::parse_url();
 
         foreach ($routes as $router) {
 
             $patterns = [
                 '{url}' => '([0-9a-zA-Z]+)',
-                '{id}' => '([0-9]+)'
+                '{id}' => '([0-9]+)',
+                '{productSlug}' => '([a-z0-9-&]+)'
             ];
 
             $url = str_replace(array_keys($patterns), array_values($patterns), $router['url']);
@@ -108,36 +112,37 @@ class Router
                 unset($parameters[0]);
                 $controllerClassName = $router['class'];
                 $controllerFile = __DIR__ . '/controller/' . $controllerClassName . '.php';
-                $functionName=$router['function'];
-                $routeType=$router['type'];
-                //call_user_func_array(new $controllerFile, $route['function'], $parameters);
+                $functionName = $router['function'];
+                $routeType = $router['type'];
 
-                try {
-                    if ($routeType == "normal") {
-                        $templateFile = $router['template'];
-                        $templateFilePath = __DIR__ . "/view/" . $templateFile;
-                        if (!file_exists($templateFilePath)) {
-                            echo "404";
-                            exit;
-                        }
-                        require_once($controllerFile);
-                        call_user_func_array(array(new $controllerClassName, $functionName), array($templateFilePath));
-                        break;
-
-                    } else if ($routeType == "check") {
-                        call_user_func_array(array(new $controllerClassName, $functionName), $parameters);
-                        break;
+                if ($routeType == "normal") {
+                    $templateFile = $router['template'];
+                    $templateFilePath = __DIR__ . "/view/" . $templateFile;
+                    if (!file_exists($templateFilePath)) {
+                        $pageNotFoundPath = __DIR__ . "/view/404.php";
+                        require_once($pageNotFoundPath);
                     }
-                } catch (Exception $exception) {
-                    var_dump($exception->getMessage());
-                    exit;
-                }
+                    require_once($controllerFile);
+                    $match = true;
+                    $controllerClassName = '\controller\\' . $controllerClassName;
+                    call_user_func_array(array(new $controllerClassName, $functionName), array($templateFilePath, $parameters));
+                    break;
 
+                } else if ($routeType == "check") {
+                    $match = true;
+                    $controllerClassName = '\controller\\' . $controllerClassName;
+                    call_user_func_array(array(new $controllerClassName, $functionName), $parameters);
+                    break;
+                }
             }
+        }
+        if ($match == false) {
+            $pageNotFoundPath = __DIR__ . "/view/404.php";
+            require_once($pageNotFoundPath);
         }
     }
 
-    public static function parse_url()
+    public static function parse_url(): array|string
     {
         $dirname = dirname($_SERVER['SCRIPT_NAME']);
         $dirname = $dirname != '/' ? $dirname : null;
