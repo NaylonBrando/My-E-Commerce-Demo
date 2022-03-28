78ac9236d502827b9ef5c64d0bcc9093bb62513b
Erhan Rohat Turuncu <rohatturuncu@gmail.com>
2022-03-29 02:12:25 +0300

    login ve register css tasınması ve refactor

--- admin/view/login.php
+++ admin/view/login.php
@@ -9,7 +9,7 @@
           integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
 
-    <link href="../../signin.css" rel="stylesheet">
+    <link href="../../css/signin.css" rel="stylesheet">
 </head>
 <body class="text-center">
 <main class="form-signin">
--- product_detail.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE HTML>
-<html lang="en">
-<?php include('product_detail_head.php'); ?>
-<body>
-<?php include('navigation_bar.php'); ?>
-<?php include('product_dal.php'); ?>
-<?php include('product_detail_body.php') ?>
-</body>
-</html>
