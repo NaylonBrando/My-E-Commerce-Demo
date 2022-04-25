c0d0f2fb9e62cf99109fa12e5c1e7e1c775a07bd
Erhan Rohat Turuncu <rohatturuncu@gmail.com>
2022-04-25 18:03:14 +0300

    Admin cartController name refactor

--- admin/controller/CartController.php
+++ admin/controller/CartController.php
@@ -9,7 +9,7 @@ class CartController extends AdminAbstractController
     public function deleteByProductId($productId)
     {
         $em = $this->getEntityManager()->getRepository(Cart::class);
-        $em->deleteByProductId($productId);
+        $em->deleteCartItemsByProductId($productId);
 
     }
 
